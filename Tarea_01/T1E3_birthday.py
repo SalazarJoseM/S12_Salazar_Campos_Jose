6158734816675770f114956b04634d44547dad39
Jose M. Salazar <18030722@itcelaya.edu.mx>
2021-11-04 18:12:50 -0600

    Create T1E3_birthday.py

---
+++ Tarea_01/T1E3_birthday.py
@@ -0,0 +1,5 @@
+dia = int(input("Ingresa tu dia de cumpleanios "))
+mes = int(input("Ingresa tu mes de cumpleanios en numero "))
+year = int(input("Ingresa tu anio de cumpleanios "))
+
+print("Year: {} - Month: {} - Day: {}".format(year,mes,dia))
