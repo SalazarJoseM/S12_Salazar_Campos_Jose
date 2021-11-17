f21d02c8fc2039376dc156b170f1c7ce933ba3c0
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-11-17 14:50:04 -0600

    Update T1E1_pos_neg.py

--- Tarea_01/T1E1_pos_neg.py
+++ Tarea_01/T1E1_pos_neg.py
@@ -1,4 +1,15 @@
-numero = float(input("Ingresa un numero "))
-if numero > 0: print("El numero es positivo")
-elif numero < 0: print("El numero es negativo")
-else: print("El numero es cero")
+""""
+Date:       2021-11-13
+Authors:    José Magadaleno Salazar Campos
+File:       T1E1_pos_neg.py
+Brief:      Programa que obtiene un numero entero o flotante y determina si es positivo, negativo o cero.
+Score:      85
+Version:    1.1.1
+Fixes:      Te hace falta la condición de __main__
+            PEP8 recomienda no usar múltiples declaraciones en una sola línea de código.
+"""
+
+numero = float(input("Ingresa un numero "))     
+if numero > 0: print("El numero es positivo")   # PEP8
+elif numero < 0: print("El numero es negativo") # PEP8
+else: print("El numero es cero")                # PEP8
