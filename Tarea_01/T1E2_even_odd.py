35c16dfcc19e9ac00cbb2b56c94907772f424da5
Jose M. Salazar <18030722@itcelaya.edu.mx>
2021-11-27 10:58:02 -0600

    Update T1E2_even_odd.py

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -1,3 +1,15 @@
-numero = float(input("Ingresa un numero "))
-if numero%2 == 0: print("El numero es par")
-else: print("El numero es impar")
+""""
+Date:       2021-11-13
+Authors:    José Magadaleno Salazar Campos
+File:       T1E2_even_odd.py
+Brief:      Programa que obtiene un numero entero o flotante y determina
+            si par o impar
+Score:
+Version:    A.B.1
+Fixes:
+"""
+
+if __name__ == '__main__':
+    numero = float(input("Ingresa un numero "))
+    if numero%2 == 0: print("El numero es par")
+    else: print("El numero es impar")
