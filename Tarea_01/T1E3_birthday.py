1c43e721a51ac270b7d9969c6ddee17197346851
Jose M. Salazar <18030722@itcelaya.edu.mx>
2021-11-27 10:58:09 -0600

    Update T1E3_birthday.py

--- Tarea_01/T1E3_birthday.py
+++ Tarea_01/T1E3_birthday.py
@@ -1,5 +1,16 @@
-dia = int(input("Ingresa tu dia de cumpleanios "))
-mes = int(input("Ingresa tu mes de cumpleanios en numero "))
-year = int(input("Ingresa tu anio de cumpleanios "))
+""""
+Date:       2021-11-13
+Authors:    José Magadaleno Salazar Campos
+File:       T1E3_birthday.py
+Brief:      Programa que obtiene una fecha de cumpleaños y lo imprime en pantalla
+Score:
+Version:    A.B.1
+Fixes:
+"""
 
-print("Year: {} - Month: {} - Day: {}".format(year,mes,dia))
+if __name__ == '__main__':
+    dia = int(input("Ingresa tu dia de cumpleanios "))
+    mes = int(input("Ingresa tu mes de cumpleanios en numero "))
+    year = int(input("Ingresa tu anio de cumpleanios "))
+
+    print("Year: {} - Month: {} - Day: {}".format(year,mes,dia))
