fcf6258fe8c6b7c948cdb51d0f73c647b9c5fe85
Jose M. Salazar <18030722@itcelaya.edu.mx>
2021-11-28 22:36:58 -0600

    Create T2E2_number_list.py

---
+++ Tarea_02/T2E2_number_list.py
@@ -0,0 +1,46 @@
+"""
+Date: 2021-11-16
+Authors: Jose Magdaleno Salazar Campos
+
+File: T2_E2_number_list.py
+Brief: Se crea una lista de n numeros desde el teclado y se realizan acciones como ordenar, añadir, imprimir, etc.
+Score:
+Version: A.B.1
+Fixes:
+"""
+
+
+if __name__ == '__main__':
+    lista_numeros = []
+
+    n = int(input("¿Cuantos numeros desea ingresar? "))
+    for i in range(n):
+        numero = float(input("Ingresa el numero " + str(i+1) + " "))
+        lista_numeros.append(numero)
+
+    # Paso 1: Imprimir elementos
+    print("\nElementos de la lista")
+    for elemento in lista_numeros:
+        print(elemento)
+
+    # Paso 2: Cambiar todos los elementos a tipo int
+    for i in range(len(lista_numeros)):
+        lista_numeros[i] = int(lista_numeros[i])
+    print("\n")
+    print("Lista convertida a int: ")
+    print(lista_numeros)
+
+    # Paso 3: Imprimir un elemento indicado por el usuario
+    indice = int(input("\nIngrese un indice de elemento de la lista para mostrar desde 0 hasta " + str(len(lista_numeros)-1) + " "))
+    print(lista_numeros)
+    print("El elemento seleccionado fue " + str(lista_numeros[indice]))
+
+    # Paso 4: Ordenar elementos
+    lista_numeros.sort()
+    print("\nLista ordenada")
+    print(lista_numeros)
+
+    # Paso 5: Ordenar elementos en reversa
+    lista_numeros.sort(reverse=True)
+    print("\nLista ordenada en reversa")
+    print(lista_numeros)
