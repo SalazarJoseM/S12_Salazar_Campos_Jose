4d1cdd99cb68a2c18486ef86b4a14c891a61fd82
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-12-04 00:56:31 -0600

    Update T1E2_even_odd.py

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -4,12 +4,20 @@ Authors:    José Magadaleno Salazar Campos
 File:       T1E2_even_odd.py
 Brief:      Programa que obtiene un numero entero o flotante y determina
             si par o impar
-Score:
-Version:    A.B.1
-Fixes:
+Score:      80
+Version:    1.1.1
+Fixes:      - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores
+                
+            - PEP8 recomienda no hacer múltiples declaraciones en una 
+                sola línea de código      
+                
+            - PEP8 recomienda dejar una línea en blanco al final del
+                código
 """
 
 if __name__ == '__main__':
     numero = float(input("Ingresa un numero "))
-    if numero%2 == 0: print("El numero es par")
-    else: print("El numero es impar")
+    # Seria bueno validar e indicar cuando el número sea cero
+    if numero%2 == 0: print("El numero es par")     # PEP8  PEP8
+    else: print("El numero es impar")               # PEP8  PEP8
