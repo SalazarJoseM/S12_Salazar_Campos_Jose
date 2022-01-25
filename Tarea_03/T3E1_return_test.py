9fe6c07aaea7b7a7d0937879e74cd4a84044863f
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-25 17:26:03 -0600

    Update T3E1_return_test.py

--- Tarea_03/T3E1_return_test.py
+++ Tarea_03/T3E1_return_test.py
@@ -1,12 +1,13 @@
 """
-Date: 2021-11-21
-Authors: Jose Magdaleno Salazar Campos
-
-File: T3E1_return_test.py
-Brief: La funcion pide un numero y regresa un string con el numero introducido.
-Score:
-Version: A.B.1
-Fixes:
+Date:       2021-11-21
+Authors:    Jose Magdaleno Salazar Campos
+File:       T3E1_return_test.py
+Brief:      La funcion pide un numero y regresa un string con el numero introducido.
+Score:      95
+Version:    1.1.1
+Fixes:      - [5] PEP8 recomienda no rebasar los 99 carácteres en una
+                línea de código, yo establecí en los requerimientos
+                máximo 72 carácteres (aplica para comentarios también)
 """
 
 
